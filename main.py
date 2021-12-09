15c4411947b53df7930989b84c4bd59db474fc29
saar2666 <saar2666@gmail.comgit config --global user.name saar2666>
2021-12-09 11:05:21 +0200

    3

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
-#prod
 import requests
 import json
 
-def exchangeProd():
-    url = 'http://api.exchangeratesapi.io/v1/latest?access_key=36d4d7b23910dc84442f4bc147637fab'
-    response = requests.get(url).json()
+
+def proExchenage():
+    currency_names = []
+    response = requests.get('http://api.exchangeratesapi.io/v1/latest?access_key=36d4d7b23910dc84442f4bc147637fab').json()
     dic = response["rates"]
-    d = dict((k, v) for k, v in dic.items() if v <= 10)
-
-    return(d)
-
-print(exchangeProd())
-
+    for k,v in dic.items():
+        if v<10:
+            currency_names.append(k)
+    return(currency_names)
 
 
-#dev
+print(proExchenage())
 
-#def exchangeDev():
+currency_names = []
+response = requests.get('http://api.exchangeratesapi.io/v1/latest?access_key=36d4d7b23910dc84442f4bc147637fab').json()
+dic = response["rates"]
+for k,v in dic.items():
+    if v<10:
+        currency_names.append(k)
+print(currency_names)
+
+with open('moc_data.json', 'w') as outfile:
+    json.dump(response["rates"], outfile)
