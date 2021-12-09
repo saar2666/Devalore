a1c0b925103b72c25a6ef82d382cc22325170b71
saar2666 <saar2666@gmail.comgit config --global user.name saar2666>
2021-12-09 11:34:57 +0200

    3

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 
-def proExchenage():
+def prodExchenage():
     currency_names = []
     response = requests.get('http://api.exchangeratesapi.io/v1/latest?access_key=36d4d7b23910dc84442f4bc147637fab').json()
     dic = response["rates"]
@@ -11,16 +11,17 @@ def proExchenage():
             currency_names.append(k)
     return(currency_names)
 
+print(prodExchenage())
 
-print(proExchenage())
 
-currency_names = []
-response = requests.get('http://api.exchangeratesapi.io/v1/latest?access_key=36d4d7b23910dc84442f4bc147637fab').json()
-dic = response["rates"]
-for k,v in dic.items():
-    if v<10:
-        currency_names.append(k)
-print(currency_names)
 
-with open('moc_data.json', 'w') as outfile:
-    json.dump(response["rates"], outfile)
+
+def devExchange():
+    parsed = json.load(open("moc_data.json", "r"))
+    currency_names = []
+    for k, v in parsed.items():
+        if v < 10:
+            currency_names.append(k)
+    return(currency_names)
+
+print(devExchange())
--- test_exchange.py
+++ test_exchange.py
@@ -1,6 +1,17 @@
 import main
 import pytest
+import requests
 
+@pytest.mark.prod
 def testProd():
-    assert main.exchangeProd()
+    response = requests.get('http://api.exchangeratesapi.io/v1/latest?access_key=36d4d7b23910dc84442f4bc147637fab').json()
+    dic = response["rates"]
+    for k, v in dic.items():
+        if v > 10:
+            currency_names.append(k)
+    list = main.exchangeProd()
+
+@pytest.mark.dev
+def testDev():
+    list =  main.devExchange()
 
