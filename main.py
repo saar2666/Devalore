ef17ba4e0f64967590ca44d9ab5c4893caed8c61
saar2666 <saar2666@gmail.comgit config --global user.name saar2666>
2021-12-10 10:24:57 +0200

    3

--- main.py
+++ main.py
@@ -3,25 +3,40 @@ import json
 
 
 def prodExchenage():
-    currency_names = []
-    response = requests.get(
-        'http://api.exchangeratesapi.io/v1/latest?access_key=36d4d7b23910dc84442f4bc147637fab').json()
-    dic = response["rates"]
-    for k, v in dic.items():
+    currency_names = [] #create empty list
+    response = requests.get('http://api.exchangeratesapi.io/v1/latest?access_key=36d4d7b23910dc84442f4bc147637fab').json() #save the api response into variable
+    dic = response["rates"] #create dictionary with the rates value from the api
+    for k, v in dic.items(): ##run on api and search for bigger then 10 element
         if v < 10:
-            currency_names.append(k)
+            currency_names.append(k) #append all the rates that are smaller then 10 as requested
     return (currency_names)
 
 
-#print(prodExchenage())
+print(prodExchenage()) #just for self testing
 
 
 def devExchange():
-    parsed = json.load(open("moc_data.json", "r"))
+    parsed = json.load(open("moc_data.json", "r")) #load the json file with the mock data we created earlier using the function below
     currency_names = []
     for k, v in parsed.items():
         if v < 10:
             currency_names.append(k)
     return (currency_names)
 
-#print(devExchange())
+print(devExchange())  #just for self testing
+
+
+
+'''
+    currency_names = []
+    response = requests.get('http://api.exchangeratesapi.io/v1/latest?access_key=36d4d7b23910dc84442f4bc147637fab').json()
+    dic = response["rates"]
+    for k,v in dic.items():
+        if v<10:
+            currency_names.append(k)
+    print(currency_names)
+    
+    with open('moc_data.json', 'w') as outfile: #create json file name mock_data.json , write permissions
+        json.dump(response["rates"], outfile) #using dump to convert object into string
+ 
+'''
--- test_exchange.py
+++ test_exchange.py
@@ -6,19 +6,19 @@ import json
 
 @pytest.mark.prod
 def testProd():
-    currency_names = []
-    response = requests.get('http://api.exchangeratesapi.io/v1/latest?access_key=36d4d7b23910dc84442f4bc147637fab').json()
-    dic = response["rates"]
-    for k, v in dic.items():
+    currency_names = [] #create empty list
+    response = requests.get('http://api.exchangeratesapi.io/v1/latest?access_key=36d4d7b23910dc84442f4bc147637fab').json() #save the api response into variable
+    dic = response["rates"] #create dictionary with the rates value from the api
+    for k, v in dic.items(): #run on api and search for bigger then 10 element
         if v > 10:
-            currency_names.append(k)
-    list = main.prodExchenage()
-    assert currency_names not in list
+            currency_names.append(k) #append all the rates that are bigger then 10
+    list = main.prodExchenage() #create a list with the real live data from prod function
+    assert currency_names not in list #if currency_names not in the >10 list (which will return true) pass the test , else , fail the test
 
 @pytest.mark.dev
 def testDev():
     currency_names = []
-    parsed = json.load(open("moc_data.json", "r"))
+    parsed = json.load(open("moc_data.json", "r")) # same like before , just with our mock_data file we generated
     for k, v in parsed.items():
         if v > 10:
             currency_names.append(k)
